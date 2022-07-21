dd032bde690b9c91b40d4b548bce5b51c723d082
chedeline <vchedeline@gmail.com>
2022-07-21 01:19:33 -0400

    adds characters folder and main_app folder

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic import ListView, DeleteView
+from django.views.generic import ListView, DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
