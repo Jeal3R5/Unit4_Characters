49826f45cb4a047b20ce74aa9c674286a972161e
Joan Renfroe <joan.renfroe@gmail.com>
2022-07-20 21:47:00 -0700

    add views

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Character, Tamagotchi, Photo
 # Create your views here.
 
 def home(request):
@@ -7,3 +8,37 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+class CharacterCreate(CreateView):
+  model = Character
+  fields = [
+    'sex', 
+    'name',
+  ]
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+
+class CharacterUpdate(UpdateView):
+  model = Character
+  fields = [
+    'sex', 
+    'name',
+  ]
+
+class CharacterDelete(DeleteView):
+  model = Character
+  success_url = '/characters/'
+
+def characters_index(request):
+  characters = Character.objects.filter(user=request.user)
+  return render(request, 'characters/index.html', {'characters':characters})
+
+def character_detail(request, character_id):
+  character = Character.objects.get(id=character_id)
+
+  feeding_form = FeedingForm()
+  return render(request, 'characters/detail.html', {'character':character, 'feeding_form':feeding_form})
+
