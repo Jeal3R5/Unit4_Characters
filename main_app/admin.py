8891cfd5518dfcd62528db0016a41bc9f46b25fe
chedeline <vchedeline@gmail.com>
2022-07-21 09:00:26 -0400

    registers models to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Character, Tamagotchi, Feeding, Photo, Skill
 
 # Register your models here.
+admin.site.register(Character)
+admin.site.register(Tamagotchi)
+admin.site.register(Feeding)
+admin.site.register(Photo)
